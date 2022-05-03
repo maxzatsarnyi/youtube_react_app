ebba6b3b70e0d0c8fcd5e7b37af657253d4f92e8
Max Zatsarnyi <zatsarnyi.max@gmail.com>
2022-05-03 18:31:28 +0300

    App

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
 import { Suspense } from 'react';
+import { Header } from './components/Header/Header';
+import { Sidebar } from './components/Sidebar/Sidebar';
 import { Routes } from './routes/index';
 
 function App() {
   return (
     <Suspense fallback={<div>Loading...</div>}>
-      <div>
-        <Routes />
+      <div className='app'>
+        {/* HEADER */}
+        {/* Sidebar */}
+        {/* Recommended */}
+        <Header />
+        <div className='app__page'>
+          <Sidebar />
+          <div className='app__container'>
+            <Routes />
+          </div>
+        </div>
       </div>
     </Suspense>
   );
